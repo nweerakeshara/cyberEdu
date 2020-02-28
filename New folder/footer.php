3db58e4cfbe9422438c974393b8ea6462adbe919
nweerakeshara <thnuwanga@gmail.com>
2020-02-28 18:13:58 +0530

    footer done

--- New folder/footer.php
+++ New folder/footer.php
@@ -60,38 +60,3 @@
 	<script src="js/owl.carousel.min.js"></script>
 	<script src="js/jquery.stellar.min.js"></script>
 	<script src="js/main.js"></script> -->
-
-
-<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
-<script src="assets/js/modernizr-latest.js"></script>
-<script type='text/javascript' src='assets/js/jquery.min.js'></script>
-<script type='text/javascript' src='assets/js/fancybox/jquery.fancybox.pack.js'></script>
-
-<script type='text/javascript' src='assets/js/jquery.mobile.customized.min.js'></script>
-<script type='text/javascript' src='assets/js/jquery.easing.1.3.js'></script>
-<script type='text/javascript' src='assets/js/camera.min.js'></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/custom.js"></script>
-<script>
-jQuery(function(){
-
-    jQuery('#camera_wrap_4').camera({
-        transPeriod: 500,
-        time: 3000,
-        height: '600',
-        loader: 'false',
-        pagination: true,
-        thumbnails: false,
-        hover: false,
-        playPause: false,
-        navigation: false,
-        opacityOnGrid: false,
-        imagePath: 'assets/images/'
-    });
-
-});
-
-</script>
-
-</body>
-</html>
